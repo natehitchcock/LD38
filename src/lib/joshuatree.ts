18684dc5f62171527962dc1cef47802abcb98068
Josh <joshuagalvin@gmail.com>
2017-04-21 20:15:02 -0700

    joshuatree

---
+++ src/lib/joshuatree.ts
@@ -0,0 +1,120 @@
+export class Uint64 {
+    data: Uint32Array;
+
+    constructor(data: number[] = [0, 0]) {
+        this.data = new Uint32Array(data);
+    }
+
+    Copy(from: Uint64): Uint64 {
+        this.data[0] = from.data[0];
+        this.data[1] = from.data[1];
+        return this;
+    }
+
+    Xor(target: Uint64): Uint64 {
+         return new Uint64([this.data[0] ^ target.data[0], this.data[1] ^ target.data[1]]);
+    }
+
+    Not(): Uint64 {
+        return new Uint64([~this.data[0], ~this.data[1]]);
+    }
+
+    Or(target: Uint64): Uint64 {
+        return new Uint64([this.data[0] | target.data[0], this.data[1] | target.data[1]]);
+    }
+
+    And(target: Uint64): Uint64 {
+        return new Uint64([this.data[0] & target.data[0], this.data[1] & target.data[1]]);
+    }
+
+    Unset(location: number): Uint64 {
+        if(location > 31) {
+            location -= 32;
+            this.data[1] &= (1 << location);
+        } else {
+            this.data[0] &= (1 << location);
+        }
+
+        return this;
+    }
+
+    Set(location: number): Uint64 {
+        if(location > 31) {
+            location -= 32;
+            this.data[1] |= (1 << location);
+        } else {
+            this.data[0] |= (1 << location);
+        }
+
+        return this;
+    }
+
+    Empty(): boolean {
+        return this.data[0] === 0 && this.data[1] === 0;
+    }
+
+    Equals(target: Uint64): boolean {
+        return target.data[0] === this.data[0] && target.data[1] === this.data[1];
+    }
+}
+
+export class JoshuaTree extends Uint64 {
+    children: {[key: string]: JoshuaTree};
+    parent: JoshuaTree;
+    payload: any;
+
+    constructor(data?: number[], payload?: any) {
+        super(data);
+        this.children = {};
+        this.payload = payload;
+    }
+
+    FromJSON(json: any): JoshuaTree {
+        this.parent = json.parent;
+        this.payload = json.payload;
+        Object.keys(json.children).forEach(key => {
+            this.children[key] = new JoshuaTree().FromJSON(json['children'][key]);
+        });
+
+        return this;
+    }
+
+    Remove(key: number): boolean {
+        if(this.children[key]) {
+            const child = this.children[key];
+            this.Copy(this.Xor(new Uint64().Set(key)));
+            delete child.parent;
+            delete this.children[key];
+            return true;
+        }
+
+        return false;
+    }
+
+    ToJSON(): string {
+        return JSON.stringify(this);
+    }
+
+    Add(key: number, child: JoshuaTree, force?: boolean): boolean {
+        if(!this.children[key]) {
+            this.children[key] = child;
+            child.parent = this;
+            this.Copy(this.Or(new Uint64().Set(key)));
+            return true;
+        } else if(force) {
+            this.Remove(key);
+            this.Add(key, child);
+            return true;
+        }
+
+        return false;
+    }
+
+    ForEach(fn: (node: JoshuaTree, key?: string) => void, key?: string) {
+        fn(this, key);
+        
+        Object.keys(this.children).forEach((childKey: string) => {
+            this.children[childKey].ForEach(fn, childKey);
+        });
+    }
+}
