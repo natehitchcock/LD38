ca31250aadba6e0173d4491fb58e0270fab99313
nathan.hitchcock <tripnull@gmail.com>
2017-04-23 09:11:51 -0700

    Refactored JTreeEntity && added toggleable debug controls

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ declare const window: IGameWindow;
 const charData = require('./content/character/character.toml');
 const testLevel = require('./content/testlevel.toml');
 
-const character = new Vox(charData);
 const scene = new THREE.Scene();
 
 window.scene = scene;
@@ -23,45 +22,52 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-
-const controls = new ThirdPersonController(camera, character);
+let flyControls: FlyCharacter;
+let controls: ThirdPersonController;
 const clock = new THREE.Clock();
 
 camera.position.z = 5;
-const geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
+
 const uniforms = {
     color: {value: new THREE.Vector4(0, 1, 0, 1)},
 };
 
 const material = new THREE.MeshPhongMaterial( {color: 0xA0522D} );
 
-let mergedGeometry = new THREE.Geometry();
-
 let sphereCenter = new THREE.Vector3(80, 80, 80);
 let sphereRadius = 40;
-let jtree = new JTreeEntity();
+let jtree = new JTreeEntity(material);
+
+jtree.position.copy(new THREE.Vector3(0, 0, 0));
 jtree.generateJTreeSphere(sphereCenter, sphereRadius);
-jtree.spawnCubes((pos, extent) => {
-    var newGeometry = new THREE.BoxGeometry( extent * 2, extent * 2, extent * 2);
-    mergedGeometry.merge(newGeometry,new THREE.Matrix4().makeTranslation(pos.x, pos.y, pos.z));
-},
-new THREE.Vector3(-80, -121, -80));
+jtree.spawnCubes();
 
-let mergedMesh = new THREE.Mesh(mergedGeometry, material);
+let debugControls = true;
+if(debugControls){
+    flyControls = new FlyCharacter(camera);
+}else{
+    const character = new Vox(charData);
+    controls = new ThirdPersonController(camera, character);
+    scene.add(character);
+}
 
-scene.add(mergedMesh);
 //scene.add(new Vox(testLevel));
-scene.add(character);
+scene.add(jtree);
 scene.add(new THREE.DirectionalLight());
 scene.add(new THREE.AmbientLight());
 
 const direction = 1;
 
 const render = () => {
-    const delta = clock.getDelta();
     requestAnimationFrame(render);
-    controls.tick(delta);
-
+    
+    if(debugControls){
+        flyControls.update(clock);
+    }else{
+        const delta = clock.getDelta();
+        controls.tick(delta);
+    }
+    
     renderer.render(scene, camera);
 };
 
