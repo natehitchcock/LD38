ae2132c27f5922e94b076ea11aa7ce6beb2e3fac
nathan.hitchcock <tripnull@gmail.com>
2017-04-24 21:09:48 -0700

    Refactored controller to be more precise

--- src/FlyCharacter.ts
+++ src/FlyCharacter.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import {keys, mouse} from './lib/input';
 
-export default class FlyCharacter{
+export default class FlyCharacter {
     cameraRef: THREE.Camera;
     speed: number = 3;
     rotationSpeedX: number = 90;
@@ -15,105 +15,66 @@ export default class FlyCharacter{
     rotX: number = 0;
     rotY: number = 0;
 
-    constructor(cam: THREE.Camera){
+    constructor(cam: THREE.Camera) {
         this.cameraRef = cam;
         this.forward = new THREE.Vector3(0, 0, -1);
         this.right = new THREE.Vector3(1, 0, 0);
         this.up = new THREE.Vector3(0, 1, 0);
     }
 
-    _updateMovement(dt: number){
-        if(keys.a){
-            let rightMove = new THREE.Vector3().copy(this.right);
-            rightMove.multiplyScalar(-this.speed * dt);
+    _updateMovement(dt: number) {
+        const moveVec = new THREE.Vector3(0, 0, 0);
 
-            this.cameraRef.position.addVectors(
-                this.cameraRef.position, 
-                rightMove);
-        }
-        if(keys.d){
-            let rightMove = new THREE.Vector3().copy(this.right);
-            rightMove.multiplyScalar(this.speed * dt);
+        if(keys.a) moveVec.sub(this.right);
+        if(keys.d) moveVec.add(this.right);
+        if(keys.w) moveVec.add(this.forward);
+        if(keys.s) moveVec.sub(this.forward);
+        if(keys.e) moveVec.add(this.up);
+        if(keys.q) moveVec.sub(this.up);
 
-            this.cameraRef.position.addVectors(
-                this.cameraRef.position, 
-                rightMove);
-        }
-        if(keys.w){
-            let forwardMove = new THREE.Vector3().copy(this.forward);
-            forwardMove.multiplyScalar(this.speed * dt);
-
-            this.cameraRef.position.addVectors(
-                this.cameraRef.position, 
-                forwardMove);
-        }
-        if(keys.s){
-            let forwardMove = new THREE.Vector3().copy(this.forward);
-            forwardMove.multiplyScalar(-this.speed * dt);
-
-            this.cameraRef.position.addVectors(
-                this.cameraRef.position, 
-                forwardMove);
-        }
-
-        if(keys.e){
-            let upMove = new THREE.Vector3().copy(this.up);
-            upMove.multiplyScalar(this.speed * dt);
-
-            this.cameraRef.position.addVectors(
-                this.cameraRef.position, 
-                upMove);
-        }
-        
-        if(keys.q){
-            let upMove = new THREE.Vector3().copy(this.up);
-            upMove.multiplyScalar(-this.speed * dt);
-
-            this.cameraRef.position.addVectors(
-                this.cameraRef.position, 
-                upMove);
-        }
+        moveVec.multiplyScalar(dt * this.speed);
+        this.cameraRef.position.add(moveVec);
     }
 
-    _updateRotation(dt: number){
-        let left = 37;
-        let up = 38;
-        let right = 39;
-        let down = 40;
+    _updateRotation(dt: number) {
+        const left = 37;
+        const up = 38;
+        const right = 39;
+        const down = 40;
 
-        if(keys[up]){
+        if(keys[up]) {
             this.rotX += this.rotationSpeedX * dt;
             this.rotX %= 360;
         }
-        if(keys[down]){
+        if(keys[down]) {
             this.rotX -= this.rotationSpeedX * dt;
             this.rotX %= 360;
         }
-        if(keys[left]){
+        if(keys[left]) {
             this.rotY += this.rotationSpeedY * dt;
             this.rotY %= 360;
         }
-        if(keys[right]){
+        if(keys[right]) {
             this.rotY -= this.rotationSpeedY * dt;
             this.rotY %= 360;
         }
 
-        let degToRad = 3.14159/180;
-        let rotYRad = this.rotY * degToRad;
-        let rotXRad = this.rotX * degToRad;
+        const degToRad = 3.14159/180;
+        const rotYRad = this.rotY * degToRad;
+        const rotXRad = this.rotX * degToRad;
+
+        // Order is very important for the next three lines of camera rotation
         this.cameraRef.rotation.setFromVector3(new THREE.Vector3(0, 0, 0));
         this.cameraRef.rotateY(rotYRad);
         this.cameraRef.rotateX(rotXRad);
 
-        // [TODO] Update character orientation vectors here
         this.forward = new THREE.Vector3(-Math.sin(rotYRad), 0, -Math.cos(rotYRad));
         this.right = new THREE.Vector3(Math.cos(rotYRad), 0, -Math.sin(rotYRad));
     }
 
-    update(clock: THREE.Clock){
-        let dt = clock.getDelta();
+    update(dt: number) {
 
         this._updateMovement(dt);
         this._updateRotation(dt);
     }
-}   
+}
